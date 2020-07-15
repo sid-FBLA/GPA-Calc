3ab4b0487ecea6ce29ee8eac6c1a9e6375720752
sid-FBLA <sid20102004@gmail.com>
2020-07-15 17:01:29 -0400

    Update map.js

--- map.js
+++ map.js
@@ -70,113 +70,133 @@ const APindex = new Array();
 const HONORSindex = new Array();
 const CPindex = new Array();
 
+var a = -1;
+var b = -1;
+var c = -1;
+
+
 const classGPA = convertToLetter.map(function(subarray, index) {
   console.log(index);
   return subarray.map(function(GPA, i) {
     console.log(i);
     if (i === 0) {
+      console.log(index);
       if(GPA.toUpperCase() == "AP") {
+        a += 1;
         APindex.push(index);
       } else if(GPA.toUpperCase() == "HONORS") {
+        b += 1;
         HONORSindex.push(index);
       } else if(GPA.toUpperCase() == "CP") {
+        c += 1;
         CPindex.push(index);
       }
     }
     if (i === 1) {
-      console.log(index);
-      console.log(APindex[index]);
-      if(index = APindex[index]) {
-        console.log(convertToLetter[index][1]);
-        if (convertToLetter[APindex[index]][1] === 'A') {
-          return GPA = 5.00;
-        } else if(convertToLetter[APindex[index]][1] === 'A-') {
-          return GPA = 4.66;
-        } else if(convertToLetter[APindex[index]][1] === 'B+') {
-          return GPA = 4.33;
-        } else if(convertToLetter[APindex[index]][1] === 'B') {
-          return GPA = 4.00;
-        } else if(convertToLetter[APindex[index]][1] === 'B-') {
-          return GPA = 3.66;
-        } else if(convertToLetter[APindex[index]][1] === 'C+') {
-          return GPA = 3.33;
-        } else if(convertToLetter[APindex[index]][1] === 'C') {
-          return GPA = 3.00;
-        } else if(convertToLetter[APindex[index]][1] === 'C-') {
-          return GPA = 2.66;
-        } else if(convertToLetter[APindex[index]][1] === 'D+') {
-          return GPA = 2.33;
-        } else if(convertToLetter[APindex[index]][1] === 'D') {
-          return GPA = 2.0;
-        } else if(convertToLetter[APindex[index]][1] === 'D-') {
-          return GPA = 1.66;
-        } else if(convertToLetter[APindex[index]][1] === 'F') {
-          return GPA = 0;
+      console.log(a);
+      console.log(convertToLetter[0][1]);
+      console.log(APindex);
+      console.log(APindex[a]);
+      if (a >= 0) {
+        console.log(a);
+        console.log(index);
+        if(APindex[a] = index) {
+          if (convertToLetter[APindex[a]][1] === 'A') {
+            return GPA = 5.00;
+          } else if(convertToLetter[APindex[a]][1] === 'A-') {
+            return GPA = 4.66;
+          } else if(convertToLetter[APindex[a]][1] === 'B+') {
+            return GPA = 4.33;
+          } else if(convertToLetter[APindex[a]][1] === 'B') {
+            return GPA = 4.00;
+          } else if(convertToLetter[APindex[a]][1] === 'B-') {
+            return GPA = 3.66;
+          } else if(convertToLetter[APindex[a]][1] === 'C+') {
+            return GPA = 3.33;
+          } else if(convertToLetter[APindex[a]][1] === 'C') {
+            return GPA = 3.00;
+          } else if(convertToLetter[APindex[a]][1] === 'C-') {
+            return GPA = 2.66;
+          } else if(convertToLetter[APindex[a]][1] === 'D+') {
+            return GPA = 2.33;
+          } else if(convertToLetter[APindex[a]][1] === 'D') {
+            return GPA = 2.0;
+          } else if(convertToLetter[APindex[a]][1] === 'D-') {
+            return GPA = 1.66;
+          } else if(convertToLetter[APindex[a]][1] === 'F') {
+            return GPA = 0;
+          }
         }
       }
-      if(index = HONORSindex[index]) {
-        if (convertToLetter[HONORSindex[index]][1] === 'A') {
-          return GPA = 4.50;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'A-') {
-          return GPA = 4.16;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'B+') {
-          return GPA = 3.83;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'B') {
-          return GPA = 3.50;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'B-') {
-          return GPA = 3.16;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'C+') {
-          return GPA = 2.88;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'C') {
-          return GPA = 2.50;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'C-') {
-          return GPA = 2.16;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'D+') {
-          return GPA = 1.83;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'D') {
-          return GPA = 1.50;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'D-') {
-          return GPA = 1.16;
-        } else if(convertToLetter[HONORSindex[index]][1] === 'F') {
-          return GPA = 0;
+      if (b >= 0) {
+        if(HONORSindex[b] = index) {
+          if (convertToLetter[HONORSindex[b]][1] === 'A') {
+            return GPA = 4.50;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'A-') {
+            return GPA = 4.16;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'B+') {
+            return GPA = 3.83;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'B') {
+            return GPA = 3.50;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'B-') {
+            return GPA = 3.16;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'C+') {
+            return GPA = 2.88;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'C') {
+            return GPA = 2.50;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'C-') {
+            return GPA = 2.16;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'D+') {
+            return GPA = 1.83;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'D') {
+            return GPA = 1.50;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'D-') {
+            return GPA = 1.16;
+          } else if(convertToLetter[HONORSindex[b]][1] === 'F') {
+            return GPA = 0;
+          }
         }
       }
-      if(index = CPindex[index]) {
-        if (convertToLetter[CPindex[index]][1] === 'A') {
-          return GPA = 4.00;
-        } else if(convertToLetter[CPindex[index]][1] === 'A-') {
-          return GPA = 3.66;
-        } else if(convertToLetter[CPindex[index]][1] === 'B+') {
-          return GPA = 3.33;
-        } else if(convertToLetter[CPindex[index]][1] === 'B') {
-          return GPA = 3.00;
-        } else if(convertToLetter[CPindex[index]][1] === 'B-') {
-          return GPA = 2.66;
-        } else if(convertToLetter[CPindex[index]][1] === 'C+') {
-          return GPA = 2.33;
-        } else if(convertToLetter[CPindex[index]][1] === 'C') {
-          return GPA = 2.00;
-        } else if(convertToLetter[CPindex[index]][1] === 'C-') {
-          return GPA = 1.66;
-        } else if(convertToLetter[CPindex[index]][1] === 'D+') {
-          return GPA = 1.33;
-        } else if(convertToLetter[CPindex[index]][1] === 'D') {
-          return GPA = 1.00;
-        } else if(convertToLetter[CPindex[index]][1] === 'D-') {
-          return GPA = 0.66;
-        } else if(convertToLetter[CPindex[index]][1] === 'F') {
-          return GPA = 0;
+      if (c >= 0) {
+        if(CPindex[c] = index) {
+          if (convertToLetter[CPindex[c]][1] === 'A') {
+            return GPA = 4.00;
+          } else if(convertToLetter[CPindex[c]][1] === 'A-') {
+            return GPA = 3.66;
+          } else if(convertToLetter[CPindex[c]][1] === 'B+') {
+            return GPA = 3.33;
+          } else if(convertToLetter[CPindex[c]][1] === 'B') {
+            return GPA = 3.00;
+          } else if(convertToLetter[CPindex[c]][1] === 'B-') {
+            return GPA = 2.66;
+          } else if(convertToLetter[CPindex[c]][1] === 'C+') {
+            return GPA = 2.33;
+          } else if(convertToLetter[CPindex[c]][1] === 'C') {
+            return GPA = 2.00;
+          } else if(convertToLetter[CPindex[c]][1] === 'C-') {
+            return GPA = 1.66;
+          } else if(convertToLetter[CPindex[c]][1] === 'D+') {
+            return GPA = 1.33;
+          } else if(convertToLetter[CPindex[c]][1] === 'D') {
+            return GPA = 1.00;
+          } else if(convertToLetter[CPindex[c]][1] === 'D-') {
+            return GPA = 0.66;
+          } else if(convertToLetter[CPindex[c]][1] === 'F') {
+            return GPA = 0;
+          }
         }
       }
     }
-    // if (i === 0) {
-    //   return GPA;
-    // }
+    if (i === 0) {
+      return GPA;
+    }
   });
 });
-
+console.log(a);
 console.log(APindex);
+console.log(b)
 console.log(HONORSindex);
+console.log(c);
 console.log(CPindex);
 console.log(APindex.length);
 
