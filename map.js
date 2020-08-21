00331b7c6880e1683034477a8256db90d2e31cf7
sid-FBLA <sid20102004@gmail.com>
2020-08-21 18:50:27 -0400

    LocalStorage();

--- map.js
+++ map.js
@@ -1,3 +1,194 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  $('form').submit(function(e){
+    e.preventDefault();
+    let credits = document.querySelectorAll('[placeholder="Credits"]');
+    let difficulty = document.querySelectorAll('[placeholder="Difficulty"]');
+    let percent = document.querySelectorAll('[placeholder="%"]');
+
+    let rows = document.querySelectorAll('.row');
+    let arrayLength = rows.length;
+
+    console.log(arrayLength);
+
+    function createArray(value) {
+      let name = new Array(arrayLength);
+      for(let i = 0; i < value.length; i += 1) {
+        name[i] = value[i].value;
+      }
+      return name;
+    }
+
+    let classDifficulty = createArray(difficulty);
+    let classCredits = createArray(credits);
+    let classPercent = createArray(percent);
+
+    let totalWeighted = new Array();
+    let totalUnweighted = new Array();
+
+    console.log(classCredits);
+    console.log(classDifficulty);
+    console.log(classPercent);
+
+    const scores = new Array(arrayLength);
+    for(let i = 0; i < scores.length; i += 1) {
+      scores[i] = new Array(3);
+      scores[i][0] = classDifficulty[i];
+      scores[i][1] = parseInt(classCredits[i]);
+      scores[i][2] = parseInt(classPercent[i]);
+      if(isNaN(scores[i][2])) {
+        alert('Could not parse input, make sure you have typed a number into your percent column without any additional characters');
+        var e = new Error('Could not parse input, make sure you have typed a number into your percent column without any additional characters');
+        throw e;
+      };
+
+    };
+
+    console.log(scores);
+
+    const convertToLetter = scores.map(function(grade) {
+     return grade.map(function(letter, index) {
+      if(index == 2) {
+        if(letter > 93) {
+         return letter = 'A';
+        } else if(letter >= 90) {
+          return letter = 'A-';
+        } else if(letter >= 87) {
+          return letter = 'B+';
+        } else if(letter >= 83) {
+          return letter = 'B';
+        } else if(letter >= 80) {
+          return letter = 'B-';
+        } else if(letter >= 77) {
+          return letter = 'C+';
+        } else if(letter >= 73) {
+          return letter = 'C';
+        } else if(letter >= 70) {
+          return letter = 'C-';
+        } else if(letter >= 67) {
+          return letter = 'D+';
+        } else if(letter >= 63) {
+          return letter = 'D';
+        } else if(letter >= 60) {
+          return letter = 'D-';
+        } else if(letter < 60) {
+          return letter = 'F';
+        }
+      } else {
+        return letter;
+      }
+     });
+    });
+
+    console.log(convertToLetter);
+
+    const classGPA = convertToLetter.map(function(subarray, index) {
+      return subarray.map(function(GPA, i) {
+        if (i === 2) {
+          console.log(index);
+          if(classDifficulty[index] == "AP") {
+            if (convertToLetter [index][2] === 'A') {
+              return GPA = 5.00;
+            } else if(convertToLetter[index][2] === 'A-') {
+              return GPA = 4.66;
+            } else if(convertToLetter[index][2] === 'B+') {
+              return GPA = 4.33;
+            } else if(convertToLetter[index][2] === 'B') {
+              return GPA = 4.00;
+            } else if(convertToLetter[index][2] === 'B-') {
+              return GPA = 3.66;
+            } else if(convertToLetter[index][2] === 'C+') {
+              return GPA = 3.33;
+            } else if(convertToLetter[index][2] === 'C') {
+              return GPA = 3.00;
+            } else if(convertToLetter[index][2] === 'C-') {
+              return GPA = 2.66;
+            } else if(convertToLetter[index][2] === 'D+') {
+              return GPA = 2.33;
+            } else if(convertToLetter[index][2] === 'D') {
+              return GPA = 2.0;
+            } else if(convertToLetter[index][2] === 'D-') {
+              return GPA = 1.66;
+            } else if(convertToLetter[index][2] === 'F') {
+              return GPA = 0;
+            }
+          }
+          if(classDifficulty[index] == "HONORS") {
+            if (convertToLetter[index][2] === 'A') {
+              return GPA = 4.50;
+            } else if(convertToLetter[index][2] === 'A-') {
+              return GPA = 4.16;
+            } else if(convertToLetter[index][2] === 'B+') {
+              return GPA = 3.83;
+            } else if(convertToLetter[index][2] === 'B') {
+              return GPA = 3.50;
+            } else if(convertToLetter[index][2] === 'B-') {
+              return GPA = 3.16;
+            } else if(convertToLetter[index][2] === 'C+') {
+              return GPA = 2.88;
+            } else if(convertToLetter[index][2] === 'C') {
+              return GPA = 2.50;
+            } else if(convertToLetter[index][2] === 'C-') {
+              return GPA = 2.16;
+            } else if(convertToLetter[index][2] === 'D+') {
+              return GPA = 1.83;
+            } else if(convertToLetter[index][2] === 'D') {
+              return GPA = 1.50;
+            } else if(convertToLetter[index][2] === 'D-') {
+              return GPA = 1.16;
+            } else if(convertToLetter[index][2] === 'F') {
+              return GPA = 0;
+            }
+          }
+          if(classDifficulty[index] == "CP") {
+            if (convertToLetter[index][2] === 'A') {
+              return GPA = 4.00;
+            } else if(convertToLetter[index][2] === 'A-') {
+              return GPA = 3.66;
+            } else if(convertToLetter[index][2] === 'B+') {
+              return GPA = 3.33;
+            } else if(convertToLetter[index][2] === 'B') {
+              return GPA = 3.00;
+            } else if(convertToLetter[index][2] === 'B-') {
+              return GPA = 2.66;
+            } else if(convertToLetter[index][2] === 'C+') {
+              return GPA = 2.33;
+            } else if(convertToLetter[index][2] === 'C') {
+              return GPA = 2.00;
+            } else if(convertToLetter[index][2] === 'C-') {
+              return GPA = 1.66;
+            } else if(convertToLetter[index][2] === 'D+') {
+              return GPA = 1.33;
+            } else if(convertToLetter[index][2] === 'D') {
+              return GPA = 1.00;
+            } else if(convertToLetter[index][2] === 'D-') {
+              return GPA = 0.66;
+            } else if(convertToLetter[index][2] === 'F') {
+              return GPA = 0;
+            }
+          }
+        }
+        else {
+          return GPA;
+        }
+      });
+    });
+
+    console.log(classGPA);
+
+    for(let i = 0; i < classGPA.length; i += 1) {
+      totalWeighted[i] = classGPA[i][2];
+    }
+    console.log(totalWeighted);
+
+    const totalWeightedGPA = (totalWeighted.reduce((a, b) => a + b, 0))/arrayLength;
+
+    console.log(totalWeightedGPA);
+
+  });
+
+
+
+/*
 classGrades = new Array(8);
 var check = true;
 
@@ -181,3 +372,5 @@ console.log(CPindex);
 console.log(APindex.length);
 
 console.log(classGPA)
+*/
+});
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const form = document.querySelector('form');
   const remove = document.querySelector('#remove');
   const add = document.querySelector('#add');
+  const row7 = document.querySelector('#appender');
   const rows = document.querySelectorAll('.row');
   const lastRow = rows[rows.length - 1];
   var lastRowClone = document.querySelector('#lastRow').cloneNode(true);
@@ -22,14 +23,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 
 
-  extraInfo1.setAttribute('title', 'Semester = 2.5, Full Year = 5, Full Year + Lab = 6');
-  extraInfo2.setAttribute('title', 'Must be written as a NUMBER do not add any other symbols.' + '&#10;' + 'If you are in the second semester, take the average of your two semesters')
+  extraInfo1.setAttribute('title', 'Semester = 2.5, Full Year = 5, Course With Lab = 6');
+  extraInfo2.setAttribute('title', 'Must be written as a NUMBER')
 
   const lastRowHeight = lastRow.offsetHeight;
 
   console.log(lastRowHeight);
-
-  //console.log(document.querySelectorAll('custom-select').value);
 
   remove.addEventListener('click', function() {
     setTimeout(function(){$('#lastRow').remove();}, 225)
@@ -39,10 +38,74 @@ window.addEventListener('DOMContentLoaded', (event) => {
   });
 
   add.addEventListener('click', function() {
-    form.insertBefore(lastRowClone, form.lastElementChild);
+    insertAfter(lastRowClone, row7);
     $('#lastRow').height(0).animate({height: lastRowHeight}, 250);
     add.style.display = 'none';
     remove.style.display = 'block';
-  })
+  });
+
+  let localStorageItemLength = localStorage.length/4;
+  console.log(localStorageItemLength);
+
+  if(localStorageItemLength === 7) {
+    $('#lastRow').remove();
+    remove.style.display = 'none';
+    add.style.display = 'block';
+  };
+
+
+
+  function preSelectOption(store) {
+    for(let i = 0; i < localStorageItemLength; i += 1) {
+      let classNumber = String(i + 1);
+      let option2 = localStorage.getItem(('#class' + classNumber + store)).toString();
+      $('#class' + classNumber + store + ' option[value="' + option2 + '"]')
+      .attr('selected', 'selected');
+    }
+  }
+
+  function preSelectValue(store) {
+    for(let i = 0; i < localStorageItemLength; i += 1) {
+      let classNumber = String(i + 1);
+      let option2 = localStorage.getItem(('#class' + classNumber + store));
+      document.querySelector('#class' + classNumber + store).value = option2;
+    }
+  }
+
+  preSelectOption('Difficulty');
+  preSelectOption('Credits');
+  preSelectValue('Name');
+  preSelectValue('Percentage');
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    window.localStorage.clear();
+    console.log(localStorage);
+
+    let name = document.querySelectorAll('[placeholder="Class Name"]')
+    let credits = document.querySelectorAll('[placeholder="Credits"]');
+    let difficulty = document.querySelectorAll('[placeholder="Difficulty"]');
+    let percent = document.querySelectorAll('[placeholder="%"]');
+
+    let rows = document.querySelectorAll('.row');
+    let storageLength = rows.length;
+
+    formData('Difficulty', difficulty);
+    formData('Credits', credits);
+    formData('Name', name);
+    formData('Percentage', percent);
+
+    function formData(store, variable) {
+      for(let i = 0; i < storageLength; i += 1) {
+        let classNumber = String(i + 1);
+        localStorage.setItem('#class' + classNumber + store, variable[i].value);
+        if(storageLength === 7) {
+          window.localStorage.removeItem('#class8' + store);
+        }
+      }
+    }
+
+
+  });
 
 });
